21dfa65730a7952ea37d69ed80d04dbe58daa824
Sachin negi <sachin@technersolutions.com>
2022-04-25 14:14:24 +0530

    first commit

---
+++ Functions/sum_multi.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("The sum of 4 and 5 is:${sum(4, 5)}");
+  print("The multiply of 10 and 5 is:${multiply(10, 5)}");
+}
+
+sum(x, y) {
+  var z = x + y;
+  return z;
+}
+
+multiply(x, y) {
+  var z = x * y;
+  return z;
+}
